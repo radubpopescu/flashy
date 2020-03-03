439f0ae3b1b83c7eb3001d772ab53fd4ff837149
bernardo goes <bernardogoes@gmail.com>
2020-03-03 21:41:14 +0000

    show and index pages from deck - still in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_095552) do
+ActiveRecord::Schema.define(version: 2020_03_03_203955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_095552) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["category_id"], name: "index_decks_on_category_id"
     t.index ["user_id"], name: "index_decks_on_user_id"
   end
