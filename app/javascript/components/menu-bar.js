f2580bc16f881f54a1c1038e22a7252170def7b1
bernardo goes <bernardogoes@gmail.com>
2020-03-05 21:32:13 +0000

    none

--- app/javascript/components/menu-bar.js
+++ app/javascript/components/menu-bar.js
@@ -1,6 +1,6 @@
-$(function() {
-  // Sidebar toggle behavior
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar, #content').toggleClass('active');
-  });
-});
+document.getElementById('menu-bar').addEventListener('click', () => {
+  openNav();
+})
+const openNav = () => {
+  document.getElementById("sidebar").classList.toggle("navbar-toggler") ;
+}
