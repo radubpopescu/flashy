e08f3894ea67dbe2bb8372014d871d801db9332c
bernardo goes <bernardogoes@gmail.com>
2020-03-07 16:20:45 +0000

    again

--- app/javascript/components/menu-bar.js
+++ app/javascript/components/menu-bar.js
@@ -1,6 +1,9 @@
-document.getElementById('menu-bar').addEventListener('click', () => {
+// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+document.getElementById('menu-bar-open').addEventListener('click', () => {
   openNav();
 })
 const openNav = () => {
-  document.getElementById("sidebar").classList.toggle("navbar-toggler") ;
+  document.getElementById("mySidebar").classList.toggle("toggle-menu-bar") ;
 }
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
