77ffb485ad158466910b7b451460edef9da0ed7a
Philip OKa <philip.oka@gmail.com>
2020-03-17 20:55:41 +0000

    upvote/downvote design changed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,7 +15,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :review_value)
+    params.permit(:content, :review_value)
   end
 
   def set_deck
