9374a8fc2fd099b57f5600fc20f06762781cd5f7
bernardo goes <bernardogoes@gmail.com>
2020-03-17 21:55:06 +0000

    merging master

--- app/javascript/components/menu-bar.js
+++ app/javascript/components/menu-bar.js
@@ -1,9 +1,20 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 document.getElementById('menu-bar-open').addEventListener('click', () => {
   openNav();
-})
+});
 const openNav = () => {
   document.getElementById("mySidebar").classList.toggle("toggle-menu-bar") ;
-}
+};
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+
+
+document.querySelector('toggle-menu-bar').addEventListener('click', () => {
+  closeMenuBar();
+};
+
+const closeMenuBar = () => {
+  classList.remove('toggle-menu-bar');
+};
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,25 @@
 require("chartkick").use(require("highcharts"))
 import "bootstrap";
-import '../components/menu-bar'
 import { markCategories } from '../components/category-button'
 import { flipCard } from '../components/flip'
-import '../components/menu-bar'
 import { favoriteCategories } from '../components/favorite_category'
+import '../components/menu-bar'
+// import { openNav } from '../components/menu-bar'
+// import { closeMenuBar } from '../components/menu-bar'
 
 
 const indexCategories = document.querySelector(".categories.index");
 if (indexCategories) {
   markCategories();
   favoriteCategories();
-}
+};
 
 const flipCards = document.querySelector(".cards");
 if (flipCards) {
   flipCard();
-}
+};
+
+// const menuOpen = document.querySelectorAll('.toggle-menu-bar');
+// if (menuOpen) {
+//   closeMenuBar();
+// };
