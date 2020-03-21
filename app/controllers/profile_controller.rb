8d0a9aa68d993a5f99fb6cb8213ce89add85d37c
Philip OKa <philip.oka@gmail.com>
2020-03-21 13:04:37 +0000

    profile page done

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,8 @@
 class ProfileController < ApplicationController
   def index
     @decks = current_user.decks
+    @good_decks = @decks.select{|deck| deck.reviews.where(review_value: 1)}
+    @upvotes = @decks.map{|deck| deck.upvotes}
     @reviews = current_user.reviews
     upvoted = current_user.reviews.select{|review| review.review_value == 1}
     @upvoted_decks = upvoted.map{|reviews| reviews.deck}
