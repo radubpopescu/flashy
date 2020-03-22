1251263515aa17d2546d9969664bbd44c11247de
bernardo goes <bernardogoes@gmail.com>
2020-03-22 00:30:46 +0000

    on ...

--- app/javascript/components/category-button.js
+++ app/javascript/components/category-button.js
@@ -17,4 +17,14 @@ function markCategories() {
   );
 }
 
+
 export { markCategories } ;
+
+labels = document.querySelector(".form-group").getElementsByTagName("label").item("innerText").innerText
+
+const changeColor = (elem) => {
+  elem.style.backgroundColor="white";
+  return elem;
+};
+
+changeColor(lables);
