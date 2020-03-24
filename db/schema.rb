2889f1fbb192566230fd87141b9a8350f5b27682
Radu Popescu <radu.popescu@priceedge.se>
2020-03-24 20:12:12 +0100

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_190552) do
+ActiveRecord::Schema.define(version: 2020_03_24_175950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_190552) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
   end
 
   create_table "decks", force: :cascade do |t|
